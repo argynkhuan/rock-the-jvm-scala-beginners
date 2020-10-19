287c0d7183649ec5190239f3751c0c383ffb36ce
Argyn Khuan <argynkhuan@yandex.kz>
2020-10-19 18:42:26 +0600

    CBN vs CBV

---
+++ src/lectures/part1basics/CBNvsCBV.scala
@@ -0,0 +1,25 @@
+package lectures.part1basics
+
+object CBNvsCBV extends App {
+  def calledByValue (x: Long): Unit = {
+    println("by value: " + x) // x
+    println("by value: " + x) // x
+  }
+
+  def calledByName (x: => Long): Unit = {
+    println("by name: " + x) // System.nanoTime()
+    println("by name: " + x) // System.nanoTime()
+  }
+
+
+  calledByValue(System.nanoTime())
+  calledByName(System.nanoTime())
+
+
+  def infinite(): Int = 1 + infinite()
+  def printFirst(x: Int, y: => Int) = println(x)
+
+//  printFirst(infinite(), 34)
+  printFirst(32, infinite())
+
+}
