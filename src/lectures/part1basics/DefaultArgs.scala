79d42913df1182ddcd0a68fe941a0d4f86e661fd
Argyn Khuan <argynkhuan@yandex.kz>
2020-10-20 17:56:14 +0600

    default arguments

--- src/lectures/part1basics/DefaultArgs.scala
+++ src/lectures/part1basics/DefaultArgs.scala
@@ -3,13 +3,13 @@ package lectures.part1basics
 object DefaultArgs extends App {
 
   @scala.annotation.tailrec
-  def trFact(n: Int, acc: Int): Int =
+  def trFact(n: Int, acc: Int = 1): Int =
     if (n <= 1) acc
     else trFact(n - 1, n * acc)
 
-  val fact10 = trFact(10, 2)
+  val fact10 = trFact(10)
 
-  def savePicture(format: String = "jpg", width: Int = 1920, height: Int = 1080): Unit = println("save picture")
+  def savePicture(format: String = "jpg",  width: Int = 1920, height: Int = 1080): Unit = println("save picture")
 
   /*
     1. pass in every leading argument
