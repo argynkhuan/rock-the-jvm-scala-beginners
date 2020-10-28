9a3b76a5a0f10074b16934054cb2cd0d547f828f
Argyn Khuan <argynkhuan@yandex.kz>
2020-10-28 22:29:19 +0600

    method notations

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -4,18 +4,22 @@ object MethodNotations extends App {
 
   class Person(val name: String, favoriteMovie: String) {
     def likes(movie: String): Boolean = movie == favoriteMovie
-    def hangoutWith (person: Person) = s"${this.name} is hanging with ${person.name}"
+    def + (person: Person) = s"${this.name} is hanging with ${person.name}"
   }
+
+  val tom = new Person("Tom", "Fight Club")
 
   val mary = new Person("Mary", "Inception")
 
-  val tom = new Person("Tom", "Fight Club")
 
   println(mary.likes("Inception"))
   println(mary likes "Inception") // equivalent
 
-  println(mary hangoutWith tom)
+  println(mary + tom)
+
   // infix notation = operator notation
 
+
   // "operators" in Scala
+
 }
