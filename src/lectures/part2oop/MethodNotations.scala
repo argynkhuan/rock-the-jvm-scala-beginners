2e3f7283555e42f51330a53a984c5ebedf2ddbe5
Argyn Khuan <argynkhuan@yandex.kz>
2020-10-30 19:23:18 +0600

    infix, prefix, postfix, apply

--- src/lectures/part2oop/MethodNotations.scala
+++ src/lectures/part2oop/MethodNotations.scala
@@ -4,35 +4,45 @@ package lectures.part2oop
 object MethodNotations extends App {
 
   class Person(val name: String, favoriteMovie: String) {
-
     def likes(movie: String): Boolean = movie == favoriteMovie
-
     def +(person: Person) = s"${this.name} is hanging with ${person.name}"
+    def unary_! : String = s"$name, hello !"
+    def isAlive : Boolean = true
 
+    def apply(): String = s"Hi, my name is $name, and I like $favoriteMovie"
   }
 
-  val tom = new Person("Tom", "Fight Club")
 
   val mary = new Person("Mary", "Inception")
 
   println(mary.likes("Inception"))
-
   println(mary likes "Inception") // equivalent
-
-  println(mary + tom)
-
-  println(mary.+(tom))
-
-  println(1 + 2)
-
-  println(1.+(2))
-
-
-  
   // infix notation = operator notation
 
   // "operators" in Scala
+  val tom = new Person("Tom", "Fight Club")
+  println(mary + tom)
+  println(mary.+(tom))
 
-  // . . .
+  println(1 + 2)
+  println(1.+(2))
 
+  // all operators are methods
+  // Akka actors have ! ?
+
+  // prefix notation
+  val x = -1 // equivalent with 1.unary_-
+  val y = 1.unary_-
+
+  // unary_ prefix only works with - + ~ !
+  println(!mary)
+  println(mary.unary_!)
+
+  // postfix notation
+  println(mary.isAlive)
+  println(mary isAlive)
+
+  // apply
+  println(mary.apply())
+  println(mary()) // equivalent
 }
