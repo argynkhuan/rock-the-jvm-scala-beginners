6f58abc4cb206695c38e20f03c525134c5958cdc
Argyn Khuan <argynkhuan@yandex.kz>
2020-11-14 19:32:46 +0600

    exercises

---
+++ src/exercises/MyList.scala
@@ -0,0 +1,13 @@
+package exercises
+
+abstract class MyList {
+    /*
+
+      head = first element of the list
+      tail = remainder of the list
+      isEmpty = is this list empty
+      add(int) => new list with this element added
+      toString => a string representation of the list
+
+     */
+}
